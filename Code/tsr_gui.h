c84a53bb714337d61d650a143d9309f2405b77e1
Fran Núñez <ioennir@gmail.com>
2022-04-12 20:38:55 +0200

    Platform specific includes

--- Code/tsr_gui.h
+++ Code/tsr_gui.h
@@ -1,9 +1,12 @@
 #ifndef TSR_GUI
 #define TSR_GUI
 
-// platform windows...
 #include "imgui/imgui.h"
+
+#if (defined(_WIN64) && _WIN64)
 #include "imgui/imgui_impl_win32.h"
+#endif
+
 #include "imgui/imgui_impl_dx11.h"
 
 #endif
--- Code/tsr_platform.h
+++ Code/tsr_platform.h
@@ -1,9 +1,10 @@
 #ifndef TSR_PLATFORM
 #define TSR_PLATFORM
 
-//for now i know its windows
+#if (defined(_WIN64) && _WIN64)
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#endif
 
 #endif
 
