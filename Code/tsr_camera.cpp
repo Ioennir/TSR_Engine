6e647fa0e75086b3fc7196f59630179f4024b684
Ioennir <ioennir@gmail.com>
2022-07-19 10:25:43 +0200

    Clean camera code a bit

--- Code/tsr_camera.cpp
+++ Code/tsr_camera.cpp
@@ -78,8 +78,6 @@ void TSR_InitCamera(DirectX::XMFLOAT3 Position, DirectX::XMFLOAT3 Target, Direct
 	CameraControl::CamData.mProj = mProj;
 }
 
-//https://stackoverflow.com/questions/36779161/trap-cursor-in-window
-// Fix the camera rotation
 void UpdateCamera(float dt, CameraData * CamData)
 {
 	const float rotSpeed = 30.0f;
@@ -115,51 +113,14 @@ void UpdateCamera(float dt, CameraData * CamData)
 
 	float UpAxisRot = dir.x * rotSpeed * dt;
 	float RightAxisRot = dir.y * rotSpeed * dt;
-	//UpAxisRot = 0.0f;
 
-	//CameraControl::camRotation += {RightAxisRot, UpAxisRot, 0.0f};
-	
-	//Limit this movement as it glitches
 	CameraControl::camRotation.x = (CameraControl::camRotation.x + RightAxisRot);
 	CameraControl::camRotation.y = (CameraControl::camRotation.y + UpAxisRot) > 175.0f ? CameraControl::camRotation.y : (CameraControl::camRotation.y + UpAxisRot);
 
-	float RightRot = CameraControl::camRotation.x;//DirectX::XMConvertToRadians(CameraControl::camRotation.x);
-	float UpRot = CameraControl::camRotation.y;//DirectX::XMConvertToRadians(CameraControl::camRotation.y);
+	float RightRot = CameraControl::camRotation.x;
+	float UpRot = CameraControl::camRotation.y;
 
 	mRotation *= DirectX::XMMatrixRotationRollPitchYaw(RightRot, UpRot, 0.0f);
-	//mRotation *= DirectX::XMMatrixRotationAxis({1.0f, 0.0f, 0.0f, 0.0f}, RightRot);
-	//mRotation *= DirectX::XMMatrixRotationAxis({0.0f, 1.0f, 0.0f, 0.0f}, UpRot);
-
-	//mRotation *= DirectX::XMMatrixTranslationFromVector({-CameraControl::camPosition.x, -CameraControl::camPosition.y, -CameraControl::camPosition.z, 1.0f });
-	//DirectX::XMVECTOR cEye = DirectX::XMVector4Transform(DirectX::XMLoadFloat4(&CameraControl::camPosition), mRotation);
-	//DirectX::XMVECTOR cTarget = DirectX::XMVector4Transform(DirectX::XMLoadFloat4(&CameraControl::camTarget), mRotation);
-	//DirectX::XMVECTOR cUp = DirectX::XMVector4Transform(DirectX::XMLoadFloat4(&CameraControl::camUp), mRotation);
-	//
-	//DirectX::XMStoreFloat4(&CameraControl::camPosition, cEye);
-	//DirectX::XMStoreFloat4(&CameraControl::camTarget, cTarget);
-	//DirectX::XMStoreFloat4(&CameraControl::camUp, cUp);
-
-
-	//LOGDEBUG(LOGSYSTEM_TSR, TEXTMESSAGE("X: " + STR(UpAxisRot) + " Y: " + STR(RightAxisRot)));
-	//NOTE(Fran): rotation is funky sometimes, doing rombo movement shows the issue (diagonals)
-	
-	//DirectX::XMMATRIX rotMat = DirectX::XMMatrixRotationRollPitchYaw(yRot, xRot, 0.0f);
-	//CamData->mView *= rotMat;
-
-	// Get the local rotation axis
-	//DirectX::XMVECTOR f = DirectX::XMVector4Normalize(DirectX::XMVector4Transform({ 0.0f, 0.0f, 1.0f, 0.0f }, CamData->mView));
-	//DirectX::XMStoreFloat4(&CameraControl::camFwd, f);
-	//DirectX::XMVECTOR r = DirectX::XMVector4Normalize(DirectX::XMVector4Transform({ 1.0f, 0.0f, 0.0f, 0.0f }, CamData->mView));
-	//DirectX::XMStoreFloat4(&CameraControl::camRight, r);
-	//DirectX::XMVECTOR u = DirectX::XMVector4Normalize(DirectX::XMVector4Transform({ 0.0f, 1.0f, 0.0f, 0.0f }, CamData->mView));
-	//DirectX::XMStoreFloat4(&CameraControl::camUp, u);
-	//
-	//// y rotations will always be around world y axis XD
-	//DirectX::XMMATRIX RotY = DirectX::XMMatrixRotationAxis(u, UpAxisRot);
-	//DirectX::XMMATRIX RotX = DirectX::XMMatrixRotationAxis(r, RightAxisRot);
-	//
-	//CamData->mView *= RotX;
-	//CamData->mView *= RotY;
 
 	// TODO(Fran): Check the disable obsolete keyIO
 	bool forward = ImGui::IsKeyDown(ImGuiKey_W);
@@ -192,24 +153,6 @@ void UpdateCamera(float dt, CameraData * CamData)
 	DirectX::XMVECTOR cUp = DirectX::XMVector4Transform({ 0.0f, 1.0f, 0.0f, 0.0f }, mRT);
 	DirectX::XMStoreFloat4(&CameraControl::camUp, cUp);
 
-
-	//
-	//DirectX::XMFLOAT4 Movement = CameraControl::camPosition + MovOffset;
-	//DirectX::XMFLOAT4 Move = { Movement.x, Movement.y, Movement.z, 1.0f };
-	//
-	////mTranslation *= mRotation;
-	//mTranslation *= DirectX::XMMatrixTranslationFromVector(DirectX::XMLoadFloat4(&Move));
-	//
-	
-	//
-	//DirectX::XMVECTOR cEye = DirectX::XMVector4Transform({0.0f, 0.0f, 0.0f, 1.0f}, mRT);
-	//DirectX::XMVECTOR cTarget = DirectX::XMVector4Transform(DirectX::XMLoadFloat4(&CameraControl::camTarget), mRT);
-	//DirectX::XMVECTOR cUp = DirectX::XMVector4Transform(DirectX::XMLoadFloat4(&CameraControl::camUp), mRT);
-	//
-	//DirectX::XMStoreFloat4(&CameraControl::camPosition, cEye);
-	//DirectX::XMStoreFloat4(&CameraControl::camTarget, cTarget);
-	//DirectX::XMStoreFloat4(&CameraControl::camUp, cUp);
-	//
 	TSR_RecalculateCameraVectors(
 		{ CameraControl::camUp.x, CameraControl::camUp.y, CameraControl::camUp.z },
 		{ CameraControl::camPosition.x, CameraControl::camPosition.y, CameraControl::camPosition.z },
